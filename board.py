969cba92b3ad9489938969648aadb8f9ffb4cbd4
J1u2a3n22 <juan.canedo@nice.com>
2022-10-14 19:07:19 -0400

    Add principal methods in the class board

--- board.py
+++ board.py
@@ -25,4 +25,28 @@ class Board:
         self.number_of_ships=number_of_ships
         self.missed_shoot="-"
         self.successful_shoot="*"
-   
+
+    def get_initial_board(self):
+        for y in range(self.rows):
+            #Add a new row
+            self.board.append([])
+            for x in range(self.columns):
+                #Add into the new row a variable to identify is a sea
+                self.board[y].append(SEA)
+        return self.board
+
+    def get_random_x(self):
+        return random.randint(0, self.columns-1)
+
+    def get_random_y(self):
+        return random.randint(0, self.rows-1)
+
+    def is_in_range(self,x,y):
+        return x >= 0 and x <= self.columns-1 and y >= 0 and y <= self.rows-1
+
+    def is_sea(self,x,y):
+        return self.board[y][x]==SEA
+
+    def is_sea_with_board(self,x,y,board):
+        return board[y][x]==SEA
+    
